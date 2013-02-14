07f112d6c523b8941deb18321944e50959721a64
Alec Shallenberger <Alec@Alecs-MacBook-Pro.local>
2013-02-14 03:32:41 -0500

    Added more information tomy JavaScript

--- Shallenberger_Alec_assignment2.js
+++ Shallenberger_Alec_assignment2.js
@@ -12,13 +12,33 @@ var TVshows = [
 	"NCIS: Los Angeles",
 	"Impractical Jokers",
 ];
-
-var TVshowThoughts = [
-	"Unexpecting surprises",
-	"Very thrill drivin",
-	"A Comedic, dectective show",
-	"Over the top funny, never stop laughing",
+var Restraunt1 = [
+	"Olive Garden",
 ];
+var Restraunt2 = [
+	"Red Lobster",
+];
+var watching = true, EatingCost = 20
+
+
+//Boolean Function
+// If we are watching a TV show is true, then I can relax, else, I will go out to eat
+if (watching === true) console.log("I can relax");
+ 	else {
+		console.log("I will go out to eat");
+};
+if (Restraunt1 > Restraunt2) console.log("I will be eating at Olive Garden");
+ 	else {
+		console.log("I will be eating at Red Lobster");
+};
+//Number Function
+if (EatingCost = 20) console.log("I will be eating good tonight");
+	else {
+		console.log("I will be eating at home");
+};
+
+//String Function
+
 
 
 
