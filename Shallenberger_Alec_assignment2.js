19ebde28c1bef9359ca5fbfe9179d9c507a1435c
Alec Shallenberger <Alec@Alecs-MacBook-Pro.local>
2013-02-14 02:16:04 -0500

    Saving my Variables for my Javascript

--- Shallenberger_Alec_assignment2.js
+++ Shallenberger_Alec_assignment2.js
@@ -1,1 +1,27 @@
 alert("JavaScript works!");
+
+// Alec B. Shallenberger
+// February 14, 2013
+// SDI 1302
+// Project 2
+
+//Variables
+var TVshows = [
+	"Supernatural",
+	"Hawaii Five-0",
+	"NCIS: Los Angeles",
+	"Impractical Jokers",
+];
+
+var TVshowThoughts = [
+	"Unexpecting surprises",
+	"Very thrill drivin",
+	"A Comedic, dectective show",
+	"Over the top funny, never stop laughing",
+];
+
+
+
+	
+
+	
